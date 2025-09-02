fe60a37f2d778b6cf864cefb70a2235b3e4cb4e3
Mahima-gupta108 <guptamahima108@gmail.com>
2025-09-02 16:15:07 +0530

    Update prompt_ui.py

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,13 +1,19 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import streamlit as st
+
+import os
+
+load_dotenv() 
+
+api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")  
+
 load_dotenv()
-api_key = "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
-" 
+
 llm=HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V3.1",
     task="text-generation",
-    huggingfacehub_api_token=api_key 
+    
     )
 model=ChatHuggingFace(llm=llm)
 st.header("research tool")
@@ -15,6 +21,8 @@ user_input=st.text_input("enter your prompt here")
 if st.button('summarize'):
     result=model.invoke(user_input)
     st.write(result.content)
+
+
 
 
 
